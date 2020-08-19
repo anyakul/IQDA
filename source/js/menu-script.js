d9316edc4a3fae45d7a7747922217ae42f2b4ddf
anyakul <anyakulikova111@gmail.com>
2020-08-19 03:32:22 +0300

    делаю весь проект

--- source/js/menu-script.js
+++ source/js/menu-script.js
@@ -1,12 +1,14 @@
-var navMain = document.querySelector(".header__nav");
-var navToggle = document.querySelector(".header__button");
+'use strict';
 
-navMain.classList.remove("menu--nojs");
+var navMain = document.querySelector('.header__nav');
+var navToggle = document.querySelector('.header__button');
 
-navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("header__nav--active")) {
-    navMain.classList.remove("header__nav--active");
+navMain.classList.remove('menu--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header__nav--active')) {
+    navMain.classList.remove('header__nav--active');
   } else {
-    navMain.classList.add("header__nav--active");
+    navMain.classList.add('header__nav--active');
   }
 });
